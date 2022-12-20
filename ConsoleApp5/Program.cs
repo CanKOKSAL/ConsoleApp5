71091b0b8ea0a9a2cb2a210bfbe64fd0acbc11bc
o <o@LAPTOP-4BIGA5QO>
2022-12-20 14:32:51 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+string name = "Sütlaç";
+int price = 5;
+int count = 100;
+
+Console.WriteLine("if you want our special sütlaç you can press 1 and order");
+
+
+Student student = new Student(name, price, count);
+student.GetFullInfo();
+student.Sell();
---
+++ ConsoleApp5/class.cs
@@ -0,0 +1,26 @@
+﻿public class Student :Class
+{
+    int Value = 0;
+    public int Price = 5;
+
+
+    public Student(string name, int price, int value) : base(name, price, value)
+    {
+        Price = price;
+        Value = value;
+        Name = name;
+
+    }
+    public void Totalincome()
+    {
+        Console.WriteLine(Price - 4);
+    }
+
+    public void GetFullInfo()
+    {
+        Console.WriteLine($"Name: {this.Name}");
+        Console.WriteLine($"Value: {this.Value}");
+        Console.WriteLine($"Price: {this.Price}");
+    }
+
+}
