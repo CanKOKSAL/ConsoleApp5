4b05a90ecdf62a59aa523e792a8536785fedd2bb
CanKOKSAL <118839518+CanKOKSAL@users.noreply.github.com>
2023-03-02 15:17:10 +0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,11 +5,18 @@ const withdraw = Number(prompt("Please write amount"));
 
 
 
-if (withdraw>balance)
-console.log("In your balance you dont have that much money");
-if (withdraw<balance)
-console.log("Your operation is successful");
-if(deposit>1)
-console.log("Your operation is successful");
-if(deposit<=0)
-console.log("Please write more than 1$ amount");
+if (withdraw>balance){
+ console.log("In your balance you dont have that much money");   
+}
+
+if (withdraw<balance){
+console.log("Your operation is successful"); 
+}
+
+if(deposit>1){
+ console.log("Your operation is successful");   
+}
+if(deposit<=0){
+ console.log("Please write more than 1$ amount");   
+}
+
